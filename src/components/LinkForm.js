e1389f3954a839ceace034c13033a4228a48b1c3
eskalona17 <eskalona_17@hotmail.com>
2020-07-10 18:04:33 +0200

    saving data in firebase

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export const LinkForm = () => {
-    return (
-        <form className="card card-body">
-            <div className="form-group input-group">
-                <div className="input-group-text-bg-light">
-                    <i className="material-icons">insert_link</i>
-                </div>
-                <input type="text" className="form-control" placeholder="url" name="url"/>
-            </div>
+export const LinkForm = (props) => {
+  const initialStateValues = {
+    url: "",
+    name: "",
+    description: "",
+  };
 
-        </form>
-        )
-}
+  const [values, setValues] = useState(initialStateValues);
+
+  const handleInputChanges = e => {
+      const {name, value} = e.target;
+      setValues({
+          ...values,
+          [name]: value
+      })
+      
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addOrEditLink(values);
+  };
+  return (
+    <form onSubmit={handleSubmit} className="card card-body">
+      <div className="form-group input-group">
+        <div className="input-group-text bg-light">
+          <i className="material-icons">insert_link</i>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="url"
+          name="url"
+          onChange={handleInputChanges}
+        />
+      </div>
+      <div className="form-group input-group">
+        <div className="input-group-text bg-light">
+          <i className="material-icons">create</i>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          name="name"
+          placeholder="website name"
+          onChange={handleInputChanges}
+        />
+      </div>
+      <div className="form-group">
+        <textarea
+          name="description"
+          rows="3"
+          className="form-control"
+          placeholder="Write a description"
+          onChange={handleInputChanges}
+        ></textarea>
+      </div>
+      <button className="btn btn-warning btn-block">
+        Save
+      </button>
+    </form>
+  );
+};
 
 export default LinkForm;
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import LinkForm from "./LinkForm";
+
+import { db } from "../firebase";
 
 export const Links = () => {
-    return <h1>Links</h1>
-}
+  const addOrEditLink = async (linkObject) => {
+    await db.collection("links").doc().set(linkObject);
+    console.log('new task added')
+  };
+
+  return (
+    <>
+      <LinkForm addOrEditLink={addOrEditLink} />
+    </>
+  );
+};
 
 export default Links;
