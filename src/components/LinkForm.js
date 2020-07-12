68aa27b2dd7a380e7369bba3377b6011e520311f
eskalona17 <eskalona_17@hotmail.com>
2020-07-12 12:26:56 +0200

    updating links in the database

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
+import { db } from "../firebase";
 
 export const LinkForm = (props) => {
   const initialStateValues = {
@@ -10,20 +12,43 @@ export const LinkForm = (props) => {
 
   const [values, setValues] = useState(initialStateValues);
 
-  const handleInputChanges = e => {
-      const {name, value} = e.target;
-      setValues({
-          ...values,
-          [name]: value
-      })
-      
-  }
+  const handleInputChanges = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const validateURL = (str) => {
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!validateURL(values.url)) {
+      return toast("Invalid URL", {
+        type: "warning",
+        autoClose: 1000,
+      });
+    }
     props.addOrEditLink(values);
-    setValues({...initialStateValues})
+    setValues({ ...initialStateValues });
   };
+
+  const getLinkById = async (id) => {
+    const doc = await db.collection("links").doc(id).get();
+    setValues({ ...doc.data() });
+  };
+
+  useEffect(() => {
+    if (props.currentId === "") {
+      setValues({ ...initialStateValues });
+    } else {
+      getLinkById(props.currentId);
+    }
+  }, [props.currentId]);
+
   return (
     <form onSubmit={handleSubmit} className="card card-body">
       <div className="form-group input-group">
@@ -63,7 +88,7 @@ export const LinkForm = (props) => {
         ></textarea>
       </div>
       <button className="btn btn-warning btn-block">
-        Save
+        {props.currentId === "" ? "Save" : "Update"}
       </button>
     </form>
   );
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,26 +1,39 @@
 import React, { useEffect, useState } from "react";
 import LinkForm from "./LinkForm";
-import {toast} from 'react-toastify'
+import { toast } from "react-toastify";
 
 import { db } from "../firebase";
 
 const Links = () => {
   const [links, setLinks] = useState([]);
+  const [currentId, setCurrentId] = useState("");
 
   const addOrEditLink = async (linkObject) => {
-    await db.collection("links").doc().set(linkObject);
-    toast('New Link Added', {
-      type: 'success'
-    })
+    try {
+      if (currentId == "") {
+        await db.collection("links").doc().set(linkObject);
+        toast("New Link Added", {
+          type: "success",
+        });
+      } else {
+        await db.collection("links").doc(currentId).update(linkObject);
+        toast("Link updated succesfully", {
+          type: "info",
+        });
+        setCurrentId("");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const onDeleteLink = async (id) => {
-    if(window.confirm('are you sure you want to delete this link?')){
-      await db.collection('links').doc(id).delete();
-      toast('Link removed succesfully', {
-        type: 'error',
-        autoClose: 2000
-      })
+    if (window.confirm("are you sure you want to delete this link?")) {
+      await db.collection("links").doc(id).delete();
+      toast("Link removed succesfully", {
+        type: "error",
+        autoClose: 2000,
+      });
     }
   };
 
@@ -28,9 +41,10 @@ const Links = () => {
     db.collection("links").onSnapshot((querySnapshot) => {
       const docs = [];
       querySnapshot.forEach((doc) => {
-        docs.push({ 
-          ...doc.data(), 
-          id: doc.id });
+        docs.push({
+          ...doc.data(),
+          id: doc.id,
+        });
       });
       setLinks(docs);
     });
@@ -43,7 +57,7 @@ const Links = () => {
   return (
     <>
       <div className="col-md-4 p-2">
-        <LinkForm addOrEditLink={addOrEditLink} />
+        <LinkForm {...{ addOrEditLink, currentId, links }} />
       </div>
       <div className="col-md-8 p-2">
         {links.map((link) => (
@@ -52,13 +66,19 @@ const Links = () => {
               <div className="d-flex justify-content-between">
                 <h4>{link.name}</h4>
                 <div>
-                <i
-                  className="material-icons text-danger"
-                  onClick={() => onDeleteLink(link.id)}
-                >
-                  close
-                </i>
-              </div>
+                  <i
+                    className="material-icons text-danger"
+                    onClick={() => onDeleteLink(link.id)}
+                  >
+                    close
+                  </i>
+                  <i
+                    className="material-icons"
+                    onClick={() => setCurrentId(link.id)}
+                  >
+                    create
+                  </i>
+                </div>
               </div>
               <p>{link.description}</p>
               <a href={link.url} target="_blank" rel="noopener noreferrer">
